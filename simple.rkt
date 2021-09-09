b186e40a1ab59c85b6b4431d5e4b085f67b9945f
Paul Tarvydas <guitarvydas@gmail.com>
2021-09-08 23:27:31 -0400

    first cut case macro (mchoose)

--- simple.rkt
+++ simple.rkt
@@ -6,4 +6,44 @@
     ((2) "chose two")
     ((3) "chose three")
     (else "fail")))
-	
+
+
+(choose 0)
+(choose 1)
+(choose 2)
+(choose 3)
+(choose 4)
+
+
+(define-syntax m-or
+  (syntax-rules ()
+    [(m-or) #f]
+    [(m-or e1 e2 ...)
+     (let ([temp e1])
+       (if temp
+           temp
+           (m-or e2 ...)))]))
+
+(m-or #t)
+(m-or #f #t)
+(m-or #f 1)
+(define x 2)
+(m-or #f x)
+(m-or (= x 1) x)
+(m-or (= x 2) x)
+
+(define-syntax mchoice
+  (syntax-rules ()
+    [(mchoice x) "mchoose fail"]
+    [(mchoice x (hh ht) r ...)
+     (if (eq? x hh)
+         ht
+         (mchoice x r ...))]))
+
+(mchoice 1
+    (0 "mchose zero"))
+(mchoice 0
+    (0 "mchoose zero"))
+(mchoice 1
+    (0 "mchoose zero")
+    (1 "mchoose uno"))
